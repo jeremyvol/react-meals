a789a44c7cb9a9310da25a12e2a3f85f980a52a4
Jeremy Vollant <j.vollant@gmail.com>
2022-07-02 15:28:15 +0200

    AvailableMeals

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,8 +1,8 @@
 import classes from './Card.module.css';
 
 const Card = (props) => {
-  const classNames = `${classes.card} ${props.className}`;
-  return <div className={classNames}>{props.children}</div>;
+  // const classNames = `${classes.card} ${props.className}`;
+  return <div className={classes.card}>{props.children}</div>;
 };
 
 export default Card;
--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -4,14 +4,8 @@ import classes from './Input.module.css';
 const Input = React.forwardRef((props, ref) => {
   return (
     <div className={classes.input}>
-      <label htmlFor={props.id}>{props.label}</label>
-      <input
-        type={props.type}
-        id={props.id}
-        value={props.value}
-        onChange={props.onChange}
-        onBlur={props.onBlur}
-      />
+      <label htmlFor={props.input.id}>{props.label}</label>
+      <input {...props.input} />
     </div>
   );
 });
