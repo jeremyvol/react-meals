b1223add94981d56257f476a897cfed21b9f4fe2
Jeremy Vollant <j.vollant@gmail.com>
2022-07-02 15:47:53 +0200

    Cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,7 +1,27 @@
 import classes from './Cart.module.css';
 
 const Cart = () => {
-  return <>Cart component</>;
+  const cartItems = (
+    <ul className={classes['cart-items']}>
+      {[{ id: 'c1', name: 'Sushis', amount: 2, proce: 12.99 }].map((item) => (
+        <li>{item.name}</li>
+      ))}
+    </ul>
+  );
+
+  return (
+    <div>
+      {cartItems}
+      <div className={classes.total}>
+        <span>Total Amount</span>
+        <span>35.62</span>
+      </div>
+      <div className={classes.actions}>
+        <button className={classes['button--alt']}>Close</button>
+        <button className={classes.button}>Order</button>
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
